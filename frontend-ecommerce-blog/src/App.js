eaf0011251921f573164cdd20aa925e86adf6429
espisepi <sepinaco@gmail.com>
2020-10-14 18:43:29 +0200

    header con el logo y las opciones de amazona

--- frontend-ecommerce-blog/src/App.js
+++ frontend-ecommerce-blog/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-
 // import 'uikit/dist/css/uikit.css';
 // import UIkit from 'uikit';
-// import Icons from 'uikit/dist/js/uikit-icons';
+// import Icons from 'uikit/1dist/js/uikit-icons';
 
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomeScreen from './screens/HomeScreen';
@@ -12,10 +12,67 @@ import HomeScreen from './screens/HomeScreen';
 // UIkit.use(Icons);
 
 function App() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const openMenu = () => {
+    document.querySelector('.sidebar').classList.add('open');
+  };
+  const closeMenu = () => {
+    document.querySelector('.sidebar').classList.remove('open');
+  };
+  
   return (
     <BrowserRouter>
-      <div>
-        <Route path="/" exact={true} component={HomeScreen} />
+      <div className="grid-container">
+
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">amazona</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul className="categories">
+            <li>
+              <Link to="/category/Pants">Pants</Link>
+            </li>
+            <li>
+              <Link to="/category/Shirts">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+    
+        <main className="main">
+          <div className="content">
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+
       </div>
     </BrowserRouter>
   );
