600ea3d951d4cf56a479701c31fa10f8e4654091
espisepi <sepinaco@gmail.com>
2020-10-15 18:33:39 +0200

    reducers and constants added to the project

--- frontend-ecommerce-blog/src/reducer.js
+++ frontend-ecommerce-blog/src/reducer.js
@@ -1,14 +1,41 @@
 import { combineReducers } from 'redux';
 import {
-    productListReducer
-} from './reducers/productReducers';
-import { cartReducer } from './reducers/cartReducers';
-import { userSigninReducer } from './reducers/userReducers';
-
+    productListReducer,
+    productDetailsReducer,
+    productSaveReducer,
+    productDeleteReducer,
+    productReviewSaveReducer,
+  } from './reducers/productReducers';
+  import { cartReducer } from './reducers/cartReducers';
+  import {
+    userSigninReducer,
+    userRegisterReducer,
+    userUpdateReducer,
+  } from './reducers/userReducers';
+  import {
+    orderCreateReducer,
+    orderDetailsReducer,
+    orderPayReducer,
+    myOrderListReducer,
+    orderListReducer,
+    orderDeleteReducer,
+  } from './reducers/orderReducers';
 
 const createRootReducer = () => combineReducers({
     productList: productListReducer, // state.productList me devolvera el objeto que devuelve nuestro productListReducer
+    productDetails: productDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
+    productSave: productSaveReducer,
+    productDelete: productDeleteReducer,
+    productReviewSave: productReviewSaveReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    userUpdate: userUpdateReducer,
+    myOrderList: myOrderListReducer,
+    orderList: orderListReducer,
+    orderDelete: orderDeleteReducer,
 })
 export default createRootReducer
--- frontend-ecommerce-blog/src/reducers/userReducers.js
+++ frontend-ecommerce-blog/src/reducers/userReducers.js
@@ -14,6 +14,30 @@
     }
   }
 
+  function userUpdateReducer(state = {}, action) {
+    switch (action.type) {
+      case USER_UPDATE_REQUEST:
+        return { loading: true };
+      case USER_UPDATE_SUCCESS:
+        return { loading: false, userInfo: action.payload };
+      case USER_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+      default: return state;
+    }
+  }
+  
+  function userRegisterReducer(state = {}, action) {
+    switch (action.type) {
+      case USER_REGISTER_REQUEST:
+        return { loading: true };
+      case USER_REGISTER_SUCCESS:
+        return { loading: false, userInfo: action.payload };
+      case USER_REGISTER_FAIL:
+        return { loading: false, error: action.payload };
+      default: return state;
+    }
+  }
+
   export {
-      userSigninReducer
+      userSigninReducer, userRegisterReducer, userUpdateReducer
   }
