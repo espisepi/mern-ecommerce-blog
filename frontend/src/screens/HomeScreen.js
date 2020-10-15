7476205aa3c15031387fe37489b17cac8922be29
espisepi <sepinaco@gmail.com>
2020-10-15 21:28:30 +0200

    HomeScreen completed (not work search bar)

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,86 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { listProducts } from '../actions/productActions';
 
 function HomeScreen(props) {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const [sortOrder, setSortOrder] = useState('');
+    const category = props.match.params.id ? props.match.params.id : '';
     const productList = useSelector((state) => state.productList);
     const { products, loading, error } = productList;
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(listProducts());
+        dispatch(listProducts(category));
         return () => {};
-    }, []);
+    }, [category]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(listProducts(category, searchKeyword, sortOrder));
+      };
+    const sortHandler = (e) => {
+        setSortOrder(e.target.value);
+        dispatch(listProducts(category, searchKeyword, sortOrder));
+    };
+
     return (
         <>
-            <h1>HOME SCREEN</h1>
-            {loading ? (<div>Loading...</div>) : 
-             error ? (<div>{error}</div>) :
-            (<div className="uk-child-width-expand@s uk-text-center" data-uk-grid>
-                {products.map((product) => (
-                    <>
-                    <div>
-                        <div className="uk-card uk-card-default uk-card-body">{product.name}</div>
-                    </div>
-                    </>
-                ))}
-            </div>)
-             }
-        </>
+      {category && <h2>{category}</h2>}
+
+      <ul className="filter">
+        <li>
+          <form onSubmit={submitHandler}>
+            <input
+              name="searchKeyword"
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </li>
+        <li>
+          Sort By{' '}
+          <select name="sortOrder" onChange={sortHandler}>
+            <option value="">Newest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </li>
+      </ul>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <ul className="products">
+          {products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <Link to={'/product/' + product._id}>
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
+                </Link>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+                <div className="product-rating">
+                  {/* <Rating
+                    value={product.rating}
+                    text={product.numReviews + ' reviews'}
+                  /> */}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
     );
 }
 
